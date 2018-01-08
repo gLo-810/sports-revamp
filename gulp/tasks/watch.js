a15725f6cf43a0d5e5116dfb160998664a01c62d
glo-810 <gl042033@m1717038.northamerica.cerner.net>
2018-01-07 18:58:13 -0600

    updated gulp file with browser sync and postcss features. also trimmed out some scss files.

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,30 @@
+const gulp = require('gulp'),
+watch = require('gulp-watch'),
+browserSync = require('browser-sync').create();
+
+gulp.task('html', () => {
+  browserSync.reload();
+});
+
+gulp.task('watch', () => {
+
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: "app"
+    }
+  });
+
+  watch('./app/index.html', () => {
+    gulp.start('html');
+  });
+
+  watch('./app/assets/styles/**/*.css', () => {
+    gulp.start('cssInject');
+  });
+});
+
+gulp.task('cssInject', ['styles'], () => {
+  gulp.src('./app/temp/styles/main.css')
+    .pipe(browserSync.stream());
+});
