4c65ec917f72dd5871279bbc70c784d51a61efc5
Gabe <glopez810@gmail.com>
2018-01-12 11:54:58 -0600

    added babel and jquery

--- app/temp/scripts/App.js
+++ app/temp/scripts/App.js
@@ -65,75 +65,79 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
-// let pGrid = document.getElementsByClassName('pic-grid-container'),
-//     // textarea = document.querySelector('textarea'),
-//     baseball = document.getElementById('baseball'),
-//     football = document.getElementById('football'),
-//     display = document.getElementById('btn-display'),
-//     reset = document.getElementById('btn-reset'),
-//     mainText = document.getElementById('main-text'),
-//     names = [],
-//     numbers;
-//
-//
-// // save names without submitting
-// function saveNames() {
-//   names = mainText.value.split('\n');
-//   numbers = Array.from({length:names.length}).map((_,i)=>i); //creates a numbers array that is the same length as the names array, for indexing.
-// }
-// // save names to array, no submit button
-// mainText.addEventListener('blur', saveNames, false);
-//
-// // shuffle arrays
-// function shuffle(a) {
-//   for (let i = a.length; i; i--) {
-//     let j = Math.floor(Math.random() * i);
-//     [a[i - 1], a[j]] = [a[j], a[i - 1]];
-//   }
-// }
-//
-// //display images with names
-// function displayEls() {
-//   // clear content to start fresh
-//    pGrid[0].innerHTML = "";
-//   names.forEach(function(name, i) {
-//
-//       let picContainer = document.createElement('div'),
-//         newImg = document.createElement('img'),
-//         newName = document.createElement('p');
-//   // append the elements
-//     picContainer.className = "picture-frame";
-//     picContainer.appendChild(newImg);
-//     picContainer.appendChild(newName);
-//     newName.textContent = name;
-//
-//     if (baseball.checked) {
-//                newImg.src = "./assets/images/baseball/team" + numbers[i] + ".jpg";
-//              } else if (football.checked) {
-//                newImg.src = "./assets/images/football/team" + numbers[i] + ".gif";
-//              }
-//     pGrid[0].appendChild(picContainer);
-//   });
-// }
-//
-// display.addEventListener('click', function() {
-//   displayEls();
-// });
-//
-// random.addEventListener('click', function() {
-//   shuffle(names);
-//   shuffle(numbers);
-//   displayEls();
-// });
-//
-// reset.addEventListener('click', function() {
-//   pGrid[0].innerHTML = "";
-// });
+"use strict";
 
-alert("ABC13");
 
+var pGrid = document.getElementsByClassName('pic-grid-container'),
+
+// textarea = document.querySelector('textarea'),
+baseball = document.getElementById('baseball'),
+    football = document.getElementById('football'),
+    display = document.getElementById('btn-display'),
+    reset = document.getElementById('btn-reset'),
+    mainText = document.getElementById('main-text'),
+    names = [],
+    numbers = void 0;
+
+// save names without submitting
+function saveNames() {
+  names = mainText.value.split('\n');
+  numbers = Array.from({ length: names.length }).map(function (_, i) {
+    return i;
+  }); //creates a numbers array that is the same length as the names array, for indexing.
+}
+// save names to array, no submit button
+mainText.addEventListener('blur', saveNames, false);
+
+// shuffle arrays
+function shuffle(a) {
+  for (var i = a.length; i; i--) {
+    var j = Math.floor(Math.random() * i);
+    var _ref = [a[j], a[i - 1]];
+    a[i - 1] = _ref[0];
+    a[j] = _ref[1];
+  }
+}
+
+//display images with names
+function displayEls() {
+  // clear content to start fresh
+  pGrid[0].innerHTML = "";
+  names.forEach(function (name, i) {
+
+    var picContainer = document.createElement('div'),
+        newImg = document.createElement('img'),
+        newName = document.createElement('p');
+    // append the elements
+    picContainer.className = "picture-frame";
+    picContainer.appendChild(newImg);
+    picContainer.appendChild(newName);
+    newName.textContent = name;
+
+    if (baseball.checked) {
+      newImg.src = "./assets/images/baseball/team" + numbers[i] + ".jpg";
+    } else if (football.checked) {
+      newImg.src = "./assets/images/football/team" + numbers[i] + ".gif";
+    }
+    pGrid[0].appendChild(picContainer);
+  });
+}
+
+display.addEventListener('click', function () {
+  displayEls();
+});
+
+random.addEventListener('click', function () {
+  shuffle(names);
+  shuffle(numbers);
+  displayEls();
+});
+
+reset.addEventListener('click', function () {
+  pGrid[0].innerHTML = "";
+});
 
 /***/ })
 /******/ ]);
