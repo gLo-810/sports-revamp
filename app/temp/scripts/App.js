8e1b4b00bf0bb2b8919221047197d7ee10e4f109
Gabe <glopez810@gmail.com>
2018-01-14 16:56:42 -0600

    merge conflicts solved

--- app/temp/scripts/App.js
+++ app/temp/scripts/App.js
@@ -60,11 +60,7 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-<<<<<<< HEAD
-/******/ 	return __webpack_require__(__webpack_require__.s = 0);
-=======
 /******/ 	return __webpack_require__(__webpack_require__.s = 2);
->>>>>>> js-redo
 /******/ })
 /************************************************************************/
 /******/ ([
@@ -72,79 +68,6 @@
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
-
-
-<<<<<<< HEAD
-var pGrid = document.getElementsByClassName('pic-grid-container'),
-
-// textarea = document.querySelector('textarea'),
-baseball = document.getElementById('baseball'),
-    football = document.getElementById('football'),
-    display = document.getElementById('btn-display'),
-    reset = document.getElementById('btn-reset'),
-    mainText = document.getElementById('main-text'),
-    names = [],
-    numbers = void 0;
-
-// save names without submitting
-function saveNames() {
-  names = mainText.value.split('\n');
-  numbers = Array.from({ length: names.length }).map(function (_, i) {
-    return i;
-  }); //creates a numbers array that is the same length as the names array, for indexing.
-}
-// save names to array, no submit button
-mainText.addEventListener('blur', saveNames, false);
-
-// shuffle arrays
-function shuffle(a) {
-  for (var i = a.length; i; i--) {
-    var j = Math.floor(Math.random() * i);
-    var _ref = [a[j], a[i - 1]];
-    a[i - 1] = _ref[0];
-    a[j] = _ref[1];
-  }
-}
-
-//display images with names
-function displayEls() {
-  // clear content to start fresh
-  pGrid[0].innerHTML = "";
-  names.forEach(function (name, i) {
-
-    var picContainer = document.createElement('div'),
-        newImg = document.createElement('img'),
-        newName = document.createElement('p');
-    // append the elements
-    picContainer.className = "picture-frame";
-    picContainer.appendChild(newImg);
-    picContainer.appendChild(newName);
-    newName.textContent = name;
-
-    if (baseball.checked) {
-      newImg.src = "./assets/images/baseball/team" + numbers[i] + ".jpg";
-    } else if (football.checked) {
-      newImg.src = "./assets/images/football/team" + numbers[i] + ".gif";
-    }
-    pGrid[0].appendChild(picContainer);
-  });
-}
-
-display.addEventListener('click', function () {
-  displayEls();
-});
-
-random.addEventListener('click', function () {
-  shuffle(names);
-  shuffle(numbers);
-  displayEls();
-});
-
-reset.addEventListener('click', function () {
-  pGrid[0].innerHTML = "";
-});
-
-=======
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
@@ -10598,6 +10521,5 @@ var Display = function (_SaveInput) {
 
 exports.default = Display;
 
->>>>>>> js-redo
 /***/ })
 /******/ ]);
